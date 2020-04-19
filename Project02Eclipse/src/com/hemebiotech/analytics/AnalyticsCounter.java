7b96911da9cd5e33048167916ee7e9eba546ef09
David <contact@davidzair-dev.com>
2020-04-19 17:54:09 +0200

    Refactoring du code, suppression des commentaires et variables non-nécessaires d'Alex et ajout de la JavaDoc respectant les standards de développement de Caroline.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,38 +1,31 @@
 package com.hemebiotech.analytics;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 	
 	public static void main(String args[]) throws Exception {
 		//Extract data from file
-
 		ReadSymptomDataFromFile readSymptoms = new ReadSymptomDataFromFile("symptoms.txt");
 		ArrayList<String> symptoms = (ArrayList<String>) readSymptoms.GetSymptoms();
 
 		//Create data structure for results count
 		Map<String, Integer> resultsSorted = new TreeMap<String, Integer>();
 
-		//Iterate over the gathered list
-		for (Iterator<String> it = symptoms.iterator(); it.hasNext();) {
-
-		//Get current element from iterator
-		String element = it.next();
-
-		if (!resultsSorted.keySet().contains(element))
-		resultsSorted.put(element, 1);
-
-		else
-		resultsSorted.put(element, resultsSorted.get(element) + 1);
-		}
+		calculateSymp(symptoms, resultsSorted);
 		
+		generateFile(resultsSorted);
+	}
+
+	/**
+	 * @param resultsSorted Results list from the calculation
+	 */
+	private static void generateFile(Map<String, Integer> resultsSorted) throws IOException {
 		//Next generate output
 		FileWriter writer = new FileWriter("result.out");
 
@@ -47,5 +40,24 @@ public class AnalyticsCounter {
 		}
 		
 		writer.close();
+	}
+
+	/**
+	 * @param symptoms Symptoms list extract from the file 'symptoms.txt'
+	 * @param resultsSorted Results list from the calculation
+	 */
+	private static void calculateSymp(ArrayList<String> symptoms, Map<String, Integer> resultsSorted) {
+		//Iterate over the gathered list
+		for (Iterator<String> it = symptoms.iterator(); it.hasNext();) {
+
+		//Get current element from iterator
+		String element = it.next();
+
+		if (!resultsSorted.keySet().contains(element))
+		resultsSorted.put(element, 1);
+
+		else
+		resultsSorted.put(element, resultsSorted.get(element) + 1);
+		}
 	}
 }
